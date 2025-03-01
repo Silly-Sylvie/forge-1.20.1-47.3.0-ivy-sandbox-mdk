6659833b768a553a81202d3147eda1f301259b43
Silly_Sylvie <silly.sylvie.13@gmail.com>
2025-03-01 14:59:15 -0300

    grassslab item

---
+++ src/main/java/net/Silly_Sylvie/ivysandbox/block/ModBlocks.java
@@ -0,0 +1,43 @@
+package net.Silly_Sylvie.ivysandbox.block;
+
+import net.Silly_Sylvie.ivysandbox.IvySandbox;
+import net.Silly_Sylvie.ivysandbox.items.custom.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.rmi.registry.Registry;
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, IvySandbox.MOD_ID);
+
+    public static final RegistryObject<Block> GRASS_SLAB = registerBlock("grass_slab",
+            () -> new SlabBlock(BlockBehaviour.Properties.of().mapColor(MapColor.GRASS)
+                    .randomTicks().strength(0.6F).sound(SoundType.GRASS)));
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
